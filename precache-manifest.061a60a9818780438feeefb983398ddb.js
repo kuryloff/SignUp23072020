d28d2b2b271575018395b9d8cb51826b7078f6ba
Ihor Kurylov <iikurylov@gmail.com>
2020-07-24 16:53:34 +0300

    Updates

---
+++ precache-manifest.061a60a9818780438feeefb983398ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018a288f426f117b6cdfa1a59b8e5a27",
+    "url": "/SignUp23072020/index.html"
+  },
+  {
+    "revision": "c285d7ae616e4e52d369",
+    "url": "/SignUp23072020/static/css/main.454545a8.chunk.css"
+  },
+  {
+    "revision": "e92545b0c8b8c12ff8ab",
+    "url": "/SignUp23072020/static/js/2.1363290f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SignUp23072020/static/js/2.1363290f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c285d7ae616e4e52d369",
+    "url": "/SignUp23072020/static/js/main.4c2d0367.chunk.js"
+  },
+  {
+    "revision": "ccc548feb3f4d8d83da1",
+    "url": "/SignUp23072020/static/js/runtime-main.c9f3dc6b.js"
+  }
+]);
